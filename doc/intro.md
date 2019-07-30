4fb93e7973b02c91cb014363f276c529685d561f
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-07-30 14:30:50 +0530

    initial commit with criterium included

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to innovation-competition
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/innovation_competition/core_test.clj
@@ -0,0 +1,7 @@
+(ns innovation-competition.core-test
+  (:require [clojure.test :refer :all]
+            [innovation-competition.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
