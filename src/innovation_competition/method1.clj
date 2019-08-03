a9d3bc78ccc2b96fd8c9ab01ef7414f33860b26a
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-08-04 04:46:19 +0530

    the color formatting is done as well
    improving the clojure code now

--- src/innovation_competition/method1.clj
+++ src/innovation_competition/method1.clj
@@ -10,16 +10,24 @@
 
 
 (def ideas-data
-  "Represents the EDN data from `ideas.edn`"
+  "Represents the EDN data from `ideas.edn`
+  ```clojure
+   (first ideas-data)
+  ```
+  "
   (edn/read-string
    (slurp "./resources/ideas.edn")))
 
 
-;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 (def users-data
-  "Represents the EDN data from `users.edn`"
+  "Represents the EDN data from `users.edn`
+  ```clojure
+   (first users-data)
+  ```
+  "
   (edn/read-string
    (slurp "./resources/users.edn")))
 
@@ -31,23 +39,27 @@
 
 
 (def keys-in-user-data
-  "Collects all unique keys from the `users-data`"
+  "All unique keys from the `users-data`
+  "
   (set (flatten (map keys users-data))))
 
 (def keys-in-ideas-data
-  "Collects all unique keys from the `ideas-data`"
+  "All unique keys from the `ideas-data`
+  "
   (set (flatten (map keys ideas-data))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Data Transformation
+;; Data Transformation and Normalization
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; ideas-data
 
 
 (defn- average-score
-  "Util function to calculate averages."
+  "Util function to calculate averages.
+  ```clojure
+  (average-score [1 2 3 4])
+  ```
+  "
   [scores]
   (double
    (if (empty? scores)
@@ -55,7 +67,12 @@
      (/ (reduce + scores) (count scores)))))
 
 (defn- find-average-score-of-idea
-  "Calculates the average score, takes care of `nil` values. "
+  "Calculates the average score, takes care of `nil` values.
+
+  ```clojure
+  (find-average-score-of-idea  (first ideas-data))
+  ```
+  "
   [an-idea]
   (average-score
    (filter
@@ -70,21 +87,26 @@
      (assoc an-idea :average-score (find-average-score-of-idea an-idea)))
    ideas-data))
 
-
-;;;;;;;;;;;;;;;;;
-
-;; users-data
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def users-data-with-houses
-  "The `users-data` normalized for `:house` being `FreeFolk` in place of `nil`."
+  "The `users-data` normalized for `:house` being `FreeFolk` in place of `nil`.
+
+  ```clojure
+  (first users-data-with-houses)
+  ```
+  "
   (map
    (fn [a-user]
      (assoc a-user :house (:house a-user "FreeFolk")))
    users-data))
 
-(defn- make-unique-houses
-  "Function to "
+(defn- split-double-houses
+  "Function for handling the users belonging to two distinct houses.
+  ```clojure
+     (make-unique-houses (nth users-data-with-houses 8))
+  ```
+  "
   [a-user]
   (cond
     (string? (:house a-user)) a-user
@@ -93,56 +115,77 @@
                                (assoc a-user :house a-house)))))
 
 (def users-final
-  (flatten (map make-unique-houses users-data-with-houses)))
+  "Represents the `users-data` with the normalized `:house` key."
+  (flatten (map split-double-houses users-data-with-houses)))
 
 (def set-of-house-names
+  "Represents the set of all the names of the houses in the `users-final`"
   (set
    (map (fn [a-users-data]
           (name (:house a-users-data)))
         users-final)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Merging the datasets
+;; Merging the user and ideas data
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-(defn- user-info [a-user-id]
+(defn- user-info
+  "A function to query the `users-final` with a given `:id`
+  ```clojure
+  (user-info \"user-53-0008852\"))
+  ```
+  "
+  [a-user-id]
   (filter
    (fn [x]
      (= a-user-id (:id x)))
    users-final))
 
-(defn- join-ideas-and-users [an-idea]
+(defn- join-ideas-and-users
+  "Function to join the normalized `ideas` and `users` data.
+  ```clojure
+  (join-ideas-and-users (first ideas-data-with-average-scores))
+  ```
+  "
+  [an-idea]
   (let [author-id (:author-id an-idea)]
     {:idea an-idea
      :authorship (user-info author-id)}))
 
 (def ideas-and-authorship-data
+  "Represents the combination of normalized `users-data` with the `ideas-data` key."
   (map join-ideas-and-users ideas-data-with-average-scores))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Helper functions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn- authored-by-house [house-name authorship-datapoint]
+(defn- authored-by-house?
+  "Checks whether the idea has been authored by a particular house"
+  [house-name authorship-datapoint]
   (if
-   (some #{house-name} (map :house (:authorship authorship-datapoint))
-    true
-    false)))
+   (some #{house-name} (map :house (:authorship authorship-datapoint)))
+   true
+   false))
 
-(defn- ideas-by-a-house [house-name]
-  (filter #(authored-by-house house-name %)
+(defn- ideas-by-a-house
+  "Returns all the ideas which have been submitted by a particular house."
+  [house-name]
+  (filter #(authored-by-house? house-name %)
           ideas-and-authorship-data))
 
-(defn- house-info [house-name]
+(defn house-info
+  "Returns the solution relevant information for a particular house"
+  [house-name]
   (let [count-of-ideas  (count (ideas-by-a-house house-name))
-        scores-of-all-ideas (map
-                             (fn [an-idea]
-                               (get-in an-idea [:idea :scores]))
-                             (ideas-by-a-house house-name))]
+        average-scores-of-all-ideas (map
+                                     (fn [an-idea]
+                                       (get-in an-idea [:idea :average-score]))
+                                     (ideas-by-a-house house-name))]
 
     {:house-name house-name
-     :innovation-score (/ (reduce + scores-of-all-ideas) count-of-ideas)
+     :innovation-score (/ (reduce + average-scores-of-all-ideas) count-of-ideas)
      :number-of-ideas count-of-ideas}))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -157,10 +200,9 @@
 ;; DONE
 ;[ ] a list of the houses, from most innovative to least innovative
 
-(println
- (map :house-name
-      (reverse
-       (sort-by :innovation-score solution-data))))
+(map :house-name
+     (reverse
+      (sort-by :innovation-score solution-data)))
 
 ;; DONE
 ;[ ] the innovation score of each house
